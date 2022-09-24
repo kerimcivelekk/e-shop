53219fdcc14f820cf3505b5fbc8165de231e35b3
Kerim <targetbad@hotmail.com>
2022-09-24 18:58:47 +0300

    Initial Commit

---
+++ E-Shop Market Appliacation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Shop Market Appliacation
+//
+//  Created by Kerim Civelek on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
